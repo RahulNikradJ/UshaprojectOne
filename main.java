59a4ea294c87e08ea5a537fc27246b60a999a980
RahulNikradJ <RahulNikrad@gmail.com>
2023-11-27 08:52:05 +0530

    conflict 2 for fun

--- main.java
+++ main.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public static void main()
 {
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
+System.out.println("Hello cloude");
+
 }
