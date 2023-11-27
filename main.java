373156d288f9f39e2b84529b7e0b0f48770c2976
RahulNikradJ <RahulNikrad@gmail.com>
2023-11-27 08:50:49 +0530

    This is my conflict commit

--- main.java
+++ main.java
@@ -2,7 +2,4 @@ import java.util.*;
 public static void main()
 {
 System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
 }
